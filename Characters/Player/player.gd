176518e9f33c5ec7f90a9aa56b9498829c9cf218
Mary Lewis <maryilewis@gmail.com>
2024-02-06 22:55:11 -0500

    first commit

---
+++ Characters/Player/player.gd
@@ -0,0 +1,78 @@
+# player.gd
+class_name Player extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var shirt_materials = []
+
+@onready var red_mat = preload("res://Characters/character materials/shirt materials/red.tres")
+@onready var orange_mat = preload("res://Characters/character materials/shirt materials/orange.tres")
+@onready var yellow_mat = preload("res://Characters/character materials/shirt materials/yellow.tres")
+@onready var green_mat = preload("res://Characters/character materials/shirt materials/green.tres")
+@onready var blue_mat = preload("res://Characters/character materials/shirt materials/blue.tres")
+@onready var purple_mat = preload("res://Characters/character materials/shirt materials/purple.tres")
+
+# Set by the authority, synchronized on spawn.
+@export var player : int :
+	set(id):
+		player = id
+		# Give authority over the player input to the appropriate peer.
+		$PlayerInput.set_multiplayer_authority(id)
+
+@export var shirt_material: int :
+	set(mat_index):
+		shirt_material = mat_index
+		#TODO fix initial load
+		if (shirt_materials.size()):
+			$MeshInstance3D.mesh.material = shirt_materials[mat_index]
+
+# Player synchronized input.
+@onready var input = $PlayerInput
+
+func _ready():
+	# Set the camera as current if we are this player.
+	if player == multiplayer.get_unique_id():
+		$Camera3D.current = true
+		$Indicator.visible = true
+	# Only process on server.
+	# EDIT: Let the client simulate player movement too to compesate network input latency.
+	set_physics_process(multiplayer.is_server())
+	shirt_materials.append(red_mat)
+	shirt_materials.append(orange_mat)
+	shirt_materials.append(yellow_mat)
+	shirt_materials.append(green_mat)
+	shirt_materials.append(blue_mat)
+	shirt_materials.append(purple_mat)
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if input.jumping and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Reset jump state.
+	input.jumping = false
+
+	# Handle movement.
+	var direction = (transform.basis * Vector3(input.direction.x, 0, input.direction.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+# set shirt color
+func _on_shirt_color_selected(index):
+	print("set material for player ", player)
+	shirt_material = index
---
+++ multiplayer.gd
@@ -0,0 +1,72 @@
+extends Node
+
+const PORT = 4433
+
+func _ready():
+	# Start paused.
+	get_tree().paused = true
+	# You can save bandwidth by disabling server relay and peer notifications.
+	multiplayer.server_relay = false
+
+	# Automatically start the server in headless mode.
+	if DisplayServer.get_name() == "headless":
+		print("Automatically starting dedicated server.")
+		_on_create_pressed.call_deferred()
+
+
+func _on_create_pressed():
+	print("create")
+	# Start as server.
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer server.")
+		return
+	multiplayer.multiplayer_peer = peer
+	start_game()
+
+
+func _on_join_pressed():
+	print("join")
+	# Start as client.
+	var txt : String = $UI/VBoxContainer/Remote.text
+	if txt == "":
+		OS.alert("Need a remote to connect to.")
+		return
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(txt, PORT)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer client.")
+		return
+	multiplayer.multiplayer_peer = peer
+	start_game()
+
+
+func start_game():
+	print("start game")
+	# Hide the UI and unpause to start the game.
+	$UI.hide()
+	get_tree().paused = false
+	# Only change level on the server.
+	# Clients will instantiate the level via the spawner.
+	if multiplayer.is_server():
+		change_level.call_deferred(load("res://Levels/Hello/place.tscn"))
+
+# Call this function deferred and only on the main authority (server).
+func change_level(scene: PackedScene):
+	# Remove old level if any.
+	var level = $Level
+	for c in level.get_children():
+		level.remove_child(c)
+		c.queue_free()
+	# Add new level.
+	level.add_child(scene.instantiate())
+	print("add scene")
+
+
+# The server can restart the level by pressing Home.
+func _input(event):
+	if not multiplayer.is_server():
+		return
+	if event.is_action("ui_home") and Input.is_action_just_pressed("ui_home"):
+		change_level.call_deferred(load("res://Levels/Hello/place.tscn"))
