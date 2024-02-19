06bf81931b8c1ff6b1b2e3d4b57df9f0e2047639
Mary Lewis <maryilewis@gmail.com>
2024-02-18 22:44:06 -0500

    messing with camera and env

--- Levels/Terrain/Things I Made/fire/fire.gd
+++ Levels/Terrain/Things I Made/fire/fire.gd
@@ -2,4 +2,5 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$OmniLight3D.light_energy = randf_range(.95, 1.05)
+	#$OmniLight3D.light_energy = randf_range(.95, 1.05)
+	pass
